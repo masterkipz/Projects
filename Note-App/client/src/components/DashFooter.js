c995ece48ac02b98dcad4a70e70f9a667d03178a
Clifford <alt041118@gmail.com>
2023-02-02 19:37:09 +0800

    added base react frontend

---
+++ Note-App/client/src/components/DashFooter.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const DashFooter = () => {
+  const Navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const onGoHomeClick = () => {
+    let goHomeButton = null;
+    if (pathname !== "/dash") {
+      goHomeButton = (
+        <button
+          className="dash-footer__button icon-button"
+          title="Home"
+          onClick={onGoHomeClick}
+        >
+          <FontAwesomeIcon icon={faHouse} />
+        </button>
+      );
+    }
+  };
+
+  const content = (
+    <footer className="dash-footer">
+      <p>Current User:</p>
+      <p>Current Status:</p>
+    </footer>
+  );
+  return content;
+};
+
+export default DashFooter;
